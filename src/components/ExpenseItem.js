5bda1a64f61c26fae9f198b6e5c64207a5f4cd12
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2022-07-19 22:46:18 +0300

    adding new files

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-const ExpenseItem = () => {
-  return <h2>Expense item!</h2>;
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+  const { title, amount, date } = props;
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
