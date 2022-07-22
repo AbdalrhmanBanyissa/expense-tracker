8bd34cef59e151940c3b60993964dd7306ead4ca
Abd Alrahman Bany Issa <ABanyIssa@aspire-services.org>
2022-07-22 16:18:50 +0300

    adding new lines

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const { title, amount, date } = props;
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
+  const { amount, date } = props;
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -12,6 +19,7 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
